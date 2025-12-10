9679aa763292205682cdd2555b77f01901639e16
Nikolay Petrov <nikolay.a.petrov@gmail.com>
2025-12-10 22:09:03 +0100

    2025: day-03 solution - the second part

--- 2025/day-03/src/Day03.java
+++ 2025/day-03/src/Day03.java
@@ -6,32 +6,40 @@ import java.util.ArrayList;
 import java.util.stream.Stream;
 
 public class Day03 {
-    public static int solveFirst(ArrayList<String> inputList) {
-        int sum = 0;
+    public static long solveGeneric(ArrayList<String> inputList, int len) {
+        long sum = 0;
         for (String line : inputList) {
-            // assume the last two digits are the max
-            char first = line.charAt(line.length() - 2);
-            char second = line.charAt(line.length() - 1);
-
-            for (int i = line.length() - 3; i >= 0; i--) {
+            char[] joltageArr = line.substring(line.length() - len).toCharArray();
+            for (int i = line.length() - len - 1; i >= 0; i--) {
                 char c = line.charAt(i);
-                if (c >= first) {
-                    // found a bigger number on the left side
-                    if (first > second) { // check if the right number should take the place up
-                        second = first;
-                    }
-                    // move the left number to the left
-                    first = c;
+                if (c < joltageArr[0]) {
+                    continue;
                 }
+                for (int j = 1; j < len; j++) {
+                    if (joltageArr[0] >= joltageArr[j]) {
+                        char tmp = joltageArr[0];
+                        joltageArr[0] = joltageArr[j];
+                        joltageArr[j] = tmp;
+                    } else {
+                        break;
+                    }
+                }
+                joltageArr[0] = c;
             }
-            int joltage = (first - '0') * 10 + (second - '0');
+            long joltage = Long.parseLong(new String(joltageArr));
             sum += joltage;
         }
         return sum;
     }
 
-    public static int solveSecond(ArrayList<String> inputList) {
-        return 0;
+    public static long solveFirst(ArrayList<String> inputList) {
+        int len = 2;
+        return solveGeneric(inputList, len);
+    }
+
+    public static long solveSecond(ArrayList<String> inputList) {
+        int len = 12;
+        return solveGeneric(inputList, len);
     }
 
     public static void main(String[] args) {
